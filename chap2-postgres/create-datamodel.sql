1832b0022d1bcc039ca42eb95f80a2ef65019c70
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-05-28 16:20:45 +0200

    Move datamodel creation to create-datamodel.sql.

---
+++ chap2-postgres/create-datamodel.sql
@@ -0,0 +1,10 @@
+CREATE TABLE countries (
+    country_code char(2) PRIMARY KEY,
+    country_name text UNIQUE
+);
+
+INSERT INTO countries (country_code, country_name)
+VALUES ('us', 'United States'), ('mx', 'Mexico'), ('au', 'Australia'),
+       ('gb', 'United Kingdom'), ('de', 'Germany'), ('ll', 'Loompaland');
+
+DELETE FROM countries WHERE country_code = 'll';
