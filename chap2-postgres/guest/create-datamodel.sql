d3374e93de86f8e246335b75d32c8aaefac9541f
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-05-28 17:29:08 +0200

    Outer limits

--- chap2-postgres/guest/create-datamodel.sql
+++ chap2-postgres/guest/create-datamodel.sql
@@ -24,3 +24,36 @@ VALUES ('Portland', '87200', 'us');
 UPDATE cities
 SET postal_code = '97205';
 
+CREATE TABLE venues (
+    venue_id SERIAL PRIMARY KEY,
+    name varchar(255),
+    street_address text,
+    type char(7) CHECK ( type in ('public', 'private') ) DEFAULT 'public',
+    postal_code varchar(9),
+    country_code char(2),
+    FOREIGN KEY (country_code, postal_code)
+    REFERENCES cities (country_code, postal_code) MATCH FULL;
+);
+
+INSERT INTO venues (name, postal_code, country_code)
+VALUES ('Crystal Ballroom', '97205', 'us');
+
+INSERT INTO venues (name, postal_code, country_code)
+VALUES ('Voodoo Donuts', '97205', 'us') RETURNING venue_id;
+
+CREATE TABLE events (
+    event_id SERIAL PRIMARY KEY,
+    title varchar(255),
+    starts timestamp,
+    ends timestamp,
+    venue_id integer REFERENCES venues
+);
+
+INSERT INTO events (title, starts, ends, venue_id)
+VALUES ('Larp Club', '2012-02-15 17:30:00', '2012-02-15 19:30:00', 2);
+
+INSERT INTO events (title, starts, ends)
+VALUES ('April Fools Day', '2012-04-01 00:00:00', '2012-04-01 23:59:00');
+
+INSERT INTO events (title, starts, ends)
+VALUES ('Christmas Day', '2012-12-25 00:00:00', '2012-12-25 23:59:00');
--- chap2-postgres/guest/queries.sql
+++ chap2-postgres/guest/queries.sql
@@ -1,3 +1,15 @@
 SELECT cities.*, country_name
 FROM cities INNER JOIN countries
-ON cities.country_code = countries.country_code;
+  ON cities.country_code = countries.country_code;
+
+SELECT v.venue_id, v.name, c.name
+FROM venues v INNER JOIN cities c
+  ON v.postal_code=c.postal_code AND v.country_code=c.country_code;
+
+SELECT e.title, v.name
+FROM events e JOIN venues v
+  ON e.venue_id = v.venue_id;
+
+SELECT e.title, v.name
+FROM events e LEFT JOIN venues v
+  ON e.venue_id = v.venue_id;
