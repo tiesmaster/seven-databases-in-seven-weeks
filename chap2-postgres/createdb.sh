51f9e54018725c284fe75c6ed698ece422d2a220
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-05-28 11:49:45 +0200

    Add creation of countries table, and fill it.

--- chap2-postgres/createdb.sh
+++ chap2-postgres/createdb.sh
@@ -2,3 +2,14 @@
 
 createdb book
 psql book -c "CREATE EXTENSION cube;"
+
+psql book -c "CREATE TABLE countries (
+    country_code char(2) PRIMARY KEY,
+    country_name text UNIQUE
+);"
+
+psql book -c "INSERT INTO countries (country_code, country_name)
+    VALUES ('us', 'United States'), ('mx', 'Mexico'), ('au', 'Australia'),
+    ('gb', 'United Kingdom'), ('de', 'Germany'), ('ll', 'Loompaland');"
+
+psql book -c "DELETE FROM countries WHERE country_code = 'll';"
