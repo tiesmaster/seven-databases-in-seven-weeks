59986be5192ea6d05d80aa9239ea7c4c6bda85eb
Thijs Brobbel <thijsbrobbel@gmail.com>
2017-05-28 17:33:39 +0200

    End of day 1

--- chap2-postgres/guest/create-datamodel.sql
+++ chap2-postgres/guest/create-datamodel.sql
@@ -57,3 +57,9 @@ VALUES ('April Fools Day', '2012-04-01 00:00:00', '2012-04-01 23:59:00');
 
 INSERT INTO events (title, starts, ends)
 VALUES ('Christmas Day', '2012-12-25 00:00:00', '2012-12-25 23:59:00');
+
+CREATE INDEX events_title
+  ON events USING hash (title);
+
+CREATE INDEX events_starts
+  ON events USING btree (starts);
